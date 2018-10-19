0de5a1e7d036834b1c9fe3347c2e9600dd68bd4c
pkjennings999 <pkjennings999@gmail.com>
2018-10-19 02:08:06 +0100

    Fully working LCA

--- PythonApplication1/PythonApplication1/LowestCommonAncestorDAG.py
+++ PythonApplication1/PythonApplication1/LowestCommonAncestorDAG.py
@@ -1,4 +1,5 @@
 from collections import OrderedDict
+from copy import copy, deepcopy
 
 class DAGValidationError(Exception):
     pass
@@ -11,13 +12,13 @@ class DAG(object):
         """ Construct a new DAG with no nodes or edges. """
         self.graph = OrderedDict()
 
-    def add_node(self, node_name):
+    def addNode(self, node_name):
         """ Add a node if it does not exist yet, or error out. """
         if node_name in self.graph:
             return ('node %s already exists' % node_name)
         self.graph[node_name] = set()
 
-    def delete_node(self, node_name):
+    def deleteNode(self, node_name):
         """ Deletes this node and all edges referencing it. """
         if node_name not in self.graph:
             return ('node %s does not exist' % node_name)
@@ -27,150 +28,72 @@ class DAG(object):
             if node_name in edges:
                 edges.remove(node_name)
 
-    def add_edge(self, ind_node, dep_node):
+    def addEdge(self, indNode, depNode):
         """ Add an edge (dependency) between the specified nodes. """
-        if ind_node not in self.graph or dep_node not in self.graph:
+        if indNode not in self.graph or depNode not in self.graph:
             return ('one or more nodes do not exist in graph')
-        test_graph = deepcopy(self.graph)
-        test_graph[ind_node].add(dep_node)
-        is_valid, message = self.validate(test_graph)
-        if is_valid:
-            self.graph[ind_node].add(dep_node)
-        else:
-            return "error"
+        self.graph[indNode].add(depNode)
 
-    def delete_edge(self, ind_node, dep_node):
+    def deleteEdge(self, indNode, depNode):
         """ Delete an edge from the graph. """
-        if dep_node not in self.graph.get(ind_node, []):
+        if depNode not in self.graph.get(indNode, []):
             return ('this edge does not exist in graph')
-        self.graph[ind_node].remove(dep_node)
+        self.graph[indNode].remove(depNode)
 
-    #def rename_edges(self, old_task_name, new_task_name, graph=None):
-    #    """ Change references to a task in existing edges.  """
-    #    if not graph:
-    #        graph = self.graph
-    #    for node, edges in graph.items():
+    def LCA(self, firstNode, secondNode):
+        firstDict = self.getAllPredecessors(firstNode)
+        secondDict = self.getAllPredecessors(secondNode)
 
-    #        if node == old_task_name:
-    #            graph[new_task_name] = copy(edges)
-    #            del graph[old_task_name]
+        shortestDist = -1
+        lca = None
 
-    #        else:
-    #            if old_task_name in edges:
-    #                edges.remove(old_task_name)
-    #                edges.add(new_task_name)
+        while firstDict != {}:
+            (key, value) = firstDict.popitem()
+            if key in secondDict and (shortestDist > value or shortestDist == -1):
+                shortestDist = value
+                lca = key
 
-    #def predecessors(self, node, graph=None):
-    #    """ Returns a list of all predecessors of the given node """
-    #    if graph is None:
-    #        graph = self.graph
-    #    return [key for key in graph if node in graph[key]]
+        return lca
 
-    #def downstream(self, node, graph=None):
-    #    """ Returns a list of all nodes this node has edges towards.  """
-    #    if graph is None:
-    #        graph = self.graph
-    #    if node not in graph:
-    #        raise KeyError('node %s is not in graph' % node)
-    #    return list(graph[node])
+    def getAllPredecessors(self, node):
+        predecessors = {node: 0}
+        queue = {}
+        return self.getAllPredecessorsR(node, predecessors, queue, 0)
 
-    #def all_downstreams(self, node, graph=None):
-    #    """Returns a list of all nodes ultimately downstream
-    #    of the given node in the dependency graph, in
-    #    topological order."""
-    #    if graph is None:
-    #        graph = self.graph
-    #    nodes = [node]
-    #    nodes_seen = set()
-    #    i = 0
-    #    while i < len(nodes):
-    #        downstreams = self.downstream(nodes[i], graph)
-    #        for downstream_node in downstreams:
-    #            if downstream_node not in nodes_seen:
-    #                nodes_seen.add(downstream_node)
-    #                nodes.append(downstream_node)
-    #        i += 1
-    #    return list(filter(lambda node: node in nodes_seen,
-    #            self.topological_sort(graph=graph)))
+    def getAllPredecessorsR(self, node, predecessors, queue, offset):
+        res = self.predecessors(node)
+        for item in res:
+            if item not in predecessors:
+                predecessors[item] = offset + 1
+                queue[item] = offset + 1
+        if queue == {}:
+            return predecessors
+        else:
+            (key, value) = queue.popitem()
+            return self.getAllPredecessorsR(key, predecessors, queue, value)
 
-    #def all_leaves(self, graph=None):
-    #    """ Return a list of all leaves (nodes with no downstreams) """
-    #    if graph is None:
-    #        graph = self.graph
-    #    return [key for key in graph if not graph[key]]
-
-    #def from_dict(self, graph_dict):
-    #    """ Reset the graph and build it from the passed dictionary.
-    #    The dictionary takes the form of {node_name: [directed edges]}
-    #    """
-
-    #    self.reset_graph()
-    #    for new_node in six.iterkeys(graph_dict):
-    #        self.add_node(new_node)
-    #    for ind_node, dep_nodes in six.iteritems(graph_dict):
-    #        if not isinstance(dep_nodes, list):
-    #            raise TypeError('dict values must be lists')
-    #        for dep_node in dep_nodes:
-    #            self.add_edge(ind_node, dep_node)
-
-    #def reset_graph(self):
-    #    """ Restore the graph to an empty state.  """
-    #    self.graph = OrderedDict()
-
-    #def ind_nodes(self, graph=None):
-    #    """ Returns a list of all nodes in the graph with no dependencies.
-    #    """
-    #    if graph is None:
-    #        graph = self.graph
-
-    #    dependent_nodes = set(node for dependents in six.itervalues(graph) for
-    #    node in dependents)
-    #    return [node for node in graph.keys() if node not in dependent_nodes]
-
-    #def validate(self, graph=None):
-    #    """ Returns (Boolean, message) of whether DAG is valid.  """
-    #    graph = graph if graph is not None else self.graph
-    #    if len(self.ind_nodes(graph)) == 0:
-    #        return (False, 'no independent nodes detected')
-    #    try:
-    #        self.topological_sort(graph)
-    #    except ValueError:
-    #        return (False, 'failed topological sort')
-    #    return (True, 'valid')
-
-    #def topological_sort(self, graph=None):
-    #    """ Returns a topological ordering of the DAG.
-    #    Raises an error if this is not possible (graph is not valid).
-    #    """
-    #    if graph is None:
-    #        graph = self.graph
-
-    #    in_degree = {}
-    #    for u in graph:
-    #        in_degree[u] = 0
-
-    #    for u in graph:
-    #        for v in graph[u]:
-    #            in_degree[v] += 1
-
-    #    queue = deque()
-    #    for u in in_degree:
-    #        if in_degree[u] == 0:
-    #            queue.appendleft(u)
-
-    #    l = []
-    #    while queue:
-    #        u = queue.pop()
-    #        l.append(u)
-    #        for v in graph[u]:
-    #            in_degree[v] -= 1
-    #            if in_degree[v] == 0:
-    #                queue.appendleft(v)
-
-    #    if len(l) == len(graph):
-    #        return l
-    #    else:
-    #        raise ValueError('graph is not acyclic')
+    def predecessors(self, node, graph=None):
+        """ Returns a list of all predecessors of the given node """
+        if graph is None:
+            graph = self.graph
+        return [key for key in graph if node in graph[key]]
 
     def size(self):
         return len(self.graph)
+
+familyTree = DAG()
+familyTree.addNode(1)
+familyTree.addNode(2)
+familyTree.addNode(3)
+familyTree.addNode(4)
+familyTree.addNode(5)
+familyTree.addNode(6)
+familyTree.addNode(7)
+familyTree.addEdge(1, 2)
+familyTree.addEdge(1, 3)
+familyTree.addEdge(1, 4)
+familyTree.addEdge(1, 5)
+familyTree.addEdge(6, 2)
+familyTree.addEdge(2, 7)
+print(familyTree.getAllPredecessors(7))
+print(familyTree.LCA(5, 7))
